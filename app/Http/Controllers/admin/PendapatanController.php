35b90fc680d4d636f6dfa0cdeff2ef12619c0a9c
farhanariyanto03 <ariyantofarhan03@gmail.com>
2025-04-27 04:39:32 +0700

    Update: transaksi setelah kena admin

--- app/Http/Controllers/admin/PendapatanController.php
+++ app/Http/Controllers/admin/PendapatanController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Transaksi;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PendapatanController extends Controller
 {
     public function index()
     {
-        return view('pages.admin.pendapatan');
+        $transaksi = Transaksi::all()->map(function ($item) {
+            $item->jumlah_transaksi_setelah_potong = $item->jumlah_transaksi * 0.95;
+            return $item;
+        });
+
+        return view('pages.admin.pendapatan', [
+            'transaksi' => $transaksi
+        ]);
     }
 }
