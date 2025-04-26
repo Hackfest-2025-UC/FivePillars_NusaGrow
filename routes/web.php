8b0d1fad40be3f11750effd1bbf7894d8141ae2c
farhanariyanto03 <ariyantofarhan03@gmail.com>
2025-04-26 15:20:25 +0700

    Fetch: view dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\DashboardAdminController;
+use App\Http\Controllers\admin\VerifikasiAdminController;
 
-Route::get('/', function () {
-    return view('layouts.template');
-});
-Route::get('/dashboard', function () {
-    return view('pages.dashboard');
-});
+// Route::get('/', function () {
+//     return view('layouts.template');
+// });
+// Route::get('/dashboard', function () {
+//     return view('pages.dashboard');
+// });
+
+// ADMIN
+Route::get('/', [DashboardAdminController::class, 'index'])->name('admin.dashboard');
+Route::get('/verifikasi', [VerifikasiAdminController::class, 'index'])->name('admin.verifikasi');
