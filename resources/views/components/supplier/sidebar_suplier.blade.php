dc865b90b1e5c0c0f0eb661fe5347d4f98deda6c
farhanariyanto03 <ariyantofarhan03@gmail.com>
2025-04-27 06:17:32 +0700

    Update all style view

--- resources/views/components/supplier/sidebar_suplier.blade.php
+++ resources/views/components/supplier/sidebar_suplier.blade.php
@@ -5,21 +5,24 @@
         <ul class="space-y-2 font-medium">
             <li>
                 <a href="{{ route('suplier.dashboard.index') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 {{ request()->is('supplier/dashboard') ? 'bg-gray-100' : '' }}">
+                    class="flex items-center p-2 rounded-lg group
+                {{ request()->routeIs('suplier.dashboard.index') ? 'text-white dark:text-green-600 bg-green-500 dark:bg-gray-700' : 'text-gray-900 dark:text-white hover:bg-green-200 dark:hover:bg-gray-700' }}">
                     <i class="ri-dashboard-line text-2xl"></i>
                     <span class="ms-3">Beranda</span>
                 </a>
             </li>
             <li>
                 <a href="{{ route('products.index') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 {{ request()->is('supplier/products') ? 'bg-gray-100' : '' }}">
+                    class="flex items-center p-2 rounded-lg group
+                {{ request()->routeIs('products.index') ? 'text-white dark:text-green-600 bg-green-500 dark:bg-gray-700' : 'text-gray-900 dark:text-white hover:bg-green-200 dark:hover:bg-gray-700' }}">
                     <i class="ri-archive-line text-2xl"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Produk</span>
                 </a>
             </li>
             <li>
                 <a href="{{ route('suplier.request.index') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 {{ request()->is('supplier/request') ? 'bg-gray-100' : '' }}">
+                class="flex items-center p-2 rounded-lg group
+                {{ request()->routeIs('suplier.request.index') ? 'text-white dark:text-green-600 bg-green-500 dark:bg-gray-700' : 'text-gray-900 dark:text-white hover:bg-green-200 dark:hover:bg-gray-700' }}">
                     <i class="ri-question-answer-line text-2xl"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Permintaan Produsen</span>
                 </a>
