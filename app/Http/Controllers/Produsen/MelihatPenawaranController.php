8775a01f0354ec91b3cab94ea1c9dcba9e530eb8
adzazarif <adzazarf@gmail.com>
2025-04-26 16:09:35 +0700

    add midtrans

--- app/Http/Controllers/Produsen/MelihatPenawaranController.php
+++ app/Http/Controllers/Produsen/MelihatPenawaranController.php
@@ -6,13 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Models\KebutuhanPerusahan;
 use App\Models\KebutuhanProdusen;
 use App\Models\Penawaran;
+use App\Models\Produsen;
 use Illuminate\Http\Request;
 
 class MelihatPenawaranController extends Controller
 {
     public function index() {
-        $user_id = KebutuhanProdusen::where('id_users', )->first();
-        $penawarans = Penawaran::where('id_kebutuhan_perusahaan', $user_id->id_penawaran)->get();
+        $produsen = Produsen::where('id_user', 1)->first();
+        $user_id = KebutuhanProdusen::where('id_produsen', $produsen->id_produsen)->first();
+        $penawarans = Penawaran::where('id_kebutuhan_produsen', $user_id->id_kebutuhan_produsen)->get();
         return view('pages.produsen.melihat-penawaran', compact('penawarans'));
     }
 }
