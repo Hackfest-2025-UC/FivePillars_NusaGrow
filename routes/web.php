36bbc192548f479bb59d0ddd310779c80d78842c
adzazarif <adzazarf@gmail.com>
2025-04-26 13:37:47 +0700

    add controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Pengelolaan\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,5 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('pages.dashboard');
 });
+
+Route::get('/pengelolaan/dashboard', [DashboardController::class, "index"]);
