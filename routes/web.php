661362e81b4ab6a4bd638114251a375c40fb4ebe
farhanariyanto03 <ariyantofarhan03@gmail.com>
2025-04-26 22:55:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,28 +19,28 @@ Route::get('/dashboard', function () {
 });
 
 // ADMIN
-Route::prefix('admin')->group(function () {
-    Route::get('/', [DashboardAdminController::class, 'index'])->name('admin.dashboard');
-    Route::get('/verifikasi', [VerifikasiAdminController::class, 'index'])->name('admin.verifikasi');
-    Route::put('/verifikasi/kebutuhan/{id_kebutuhan_produsen}/diterima', [VerifikasiAdminController::class, 'updateStatusKebutuhanDiterima'])->name('admin.verifikasi.statusKebutuhan-terima');
-    Route::put('/verifikasi/kebutuhan/{id_kebutuhan_produsen}/ditolak', [VerifikasiAdminController::class, 'updateStatusKebutuhanDitolak'])->name('admin.verifikasi.statusKebutuhan-tolak');
-    Route::put('/verifikasi/produk/{id_produk}/diterima', [VerifikasiAdminController::class, 'updateStatusProdukDiterima'])->name('admin.verifikasi.statusProduk-terima');
-    Route::put('/verifikasi/produk/{id_produk}/ditolak', [VerifikasiAdminController::class, 'updateStatusProdukDitolak'])->name('admin.verifikasi.statusProduk-tolak');
-});
 
-Route::prefix('produsen')->group(function () {
-    Route::get('/cari-suplier', [CariSuplierController::class, "index"])->name('produsen.cari-supplier.index');
-    Route::get('/dashboard', [DashboardController::class, "index"])->name('produsen.dashboard.index');
-    Route::get('/cari-suplier', [CariSuplierController::class, "index"])->name('produsen.cari-supplier.index');
-    Route::get('melihat-penawaran', [MelihatPenawaranController::class, "index"])->name('produsen.melihat-penawaran.index');
-});
-Route::prefix('supplier')->group(function () {
-    Route::get('/dashboard', [DashboardSupplierController::class, "index"])->name('suplier.dashboard.index');
-    Route::get('/products', [ProductSupplierController::class, "index"])->name('suplier.products.index');
-    Route::get('/request', [RequestProductSupplierController::class, "index"])->name('suplier.request.index');
-});
-// Route::get('/', function () {
-//     return view('layouts.template');
+// Route::middleware('auth')->group(function () {
+
+    Route::prefix('admin')->middleware('user-role:admin')->group(function () {
+        Route::get('/', [DashboardAdminController::class, 'index'])->name('admin.dashboard');
+        Route::get('/verifikasi', [VerifikasiAdminController::class, 'index'])->name('admin.verifikasi');
+        Route::put('/verifikasi/kebutuhan/{id_kebutuhan_produsen}/diterima', [VerifikasiAdminController::class, 'updateStatusKebutuhanDiterima'])->name('admin.verifikasi.statusKebutuhan-terima');
+        Route::put('/verifikasi/kebutuhan/{id_kebutuhan_produsen}/ditolak', [VerifikasiAdminController::class, 'updateStatusKebutuhanDitolak'])->name('admin.verifikasi.statusKebutuhan-tolak');
+        Route::put('/verifikasi/produk/{id_produk}/diterima', [VerifikasiAdminController::class, 'updateStatusProdukDiterima'])->name('admin.verifikasi.statusProduk-terima');
+        Route::put('/verifikasi/produk/{id_produk}/ditolak', [VerifikasiAdminController::class, 'updateStatusProdukDitolak'])->name('admin.verifikasi.statusProduk-tolak');
+    });
+
+    Route::prefix('produsen')->group(function () {
+        Route::get('/dashboard', [DashboardController::class, "index"])->name('produsen.dashboard.index');
+        Route::get('/cari-suplier', [ProdusenController::class, "cariSuplier"])->name('produsen.cari-supplier.index');
+        Route::get('/melihat-penawaran', [ProdusenController::class, "penawaran"])->name('produsen.melihat-penawaran.index');
+    });
+    Route::prefix('supplier')->group(function () {
+        Route::get('/dashboard', [DashboardSupplierController::class, "index"])->name('suplier.dashboard.index');
+        Route::get('/products', [ProductSupplierController::class, "index"])->name('suplier.products.index');
+        Route::get('/request', [RequestProductSupplierController::class, "index"])->name('suplier.request.index');
+    });
 // });
 // Route::get('/dashboard', function () {
 //     return view('pages.dashboard');
