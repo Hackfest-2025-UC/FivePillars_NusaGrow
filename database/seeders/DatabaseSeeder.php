7bedf72b59f17d3f3ca3eb2033778a389623cbe9
rayrayaray <rayasya.dziqi@gmail.com>
2025-04-27 07:21:55 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,42 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        // User::create([
-        //     'nama' => 'Admin',
-        //     'email' => 'admin@gmail.com',
-        //     'password' => Hash::make('password'),
-        //     'role' => 'admin'
-        // ]);  
         $this->call(UserSeeder::class);
-
-        $user = User::insertGetId([
-            'nama' => 'asssssd',
-            'email' => 'asd@gmail.com',
-            'password' => Hash::make('password'),
-            'role' => 'supplier'
-        ]);
-
-        $produsen = Produsen::create([
-            'id_user' => $user,
-            'nama_produsen' => 'asd',
-            'alamat_produsen' => 'asd'
-        ]);
-
-        KebutuhanProdusen::create([
-            'id_produsen' => $produsen->id_produsen,
-            'nama_kebutuhan' => 'asd',
-            'jumlah_kebutuhan' => 2,
-            'latitude' => '23243',
-            'longitude' => '23443'
-        ]);
-
-        // $this->call(UserSeeder::class);
     }
 }
